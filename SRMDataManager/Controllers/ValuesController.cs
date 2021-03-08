77ddad602cc7682cdfde39893d629111945f8e4c
Shawn Koski <skoski@artnet.com>
2021-03-08 09:50:34 -0500

    Add the Web API Project.
    Adds a blank Web API project and tweaks it.

---
+++ SRMDataManager/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace SRMDataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            int userHashCode = RequestContext.Principal.Identity.GetHashCode();
+            System.Type userType = RequestContext.Principal.Identity.GetType();
+            string userName = RequestContext.Principal.Identity.GetUserName();
+            return new string[] { "value1", "value2", userId, userHashCode.ToString(), userType.ToString(), userName };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
