9384f16ce9374381a523ed74d9a541f1ec746df0
Shawn Koski <skoski@artnet.com>
2021-03-10 20:45:52 -0500

    Add Login and Shell Views and ViewModels
    Create basic Login UI and wire up to check if UserName box and
    Password box have values.

--- SRMDesktopUI/ViewModels/ShellViewModel.cs
+++ SRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace SRMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loinVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loinVM = loginVM;
+            ActivateItem(_loinVM);
+        }
     }
 }
